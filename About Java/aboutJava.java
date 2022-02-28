7cd1972d1565f53200a8e4343689e02b5744fc56
Joy bhattacharya <joy.arya7381@gmail.com>
2022-02-28 21:04:10 +0530

    java practices

---
+++ About Java/aboutJava.java
@@ -0,0 +1,13 @@
+class start {
+    public static void main(String args[]) {
+        System.out.println("hello joy");
+    }
+}
+
+class add {
+    public static void main(String args[]){
+       int num1 = 2, num2 = 4, sum;
+       sum = num1 + num2;
+       System.out.println(sum);
+    }
+}
